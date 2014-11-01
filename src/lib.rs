d4069379bda4a8bb79aa23563f3c38ce2c4a541f
Stanislav Panferov <fnight.m@gmail.com>
2014-11-01 14:40:35 +0300

    Simplify example

--- src/lib.rs
+++ src/lib.rs
@@ -24,29 +24,11 @@ pub struct InsertQuery<T, V, M> {
     pub values: Insert<T, V, M>
 }
 
-impl<T,V,M> InsertQuery<T,V,M> {
-    pub fn get_values(&self) -> &Insert<T, V, M> { &self.values }
-}
-
-macro_rules! to_sql_for_insert_tuple(
-    ($fmt:expr, $($t:ident, $var:ident),+) => (
-        impl<$($t,)+> ToSql for ($(ExprValue<$t>),+,)  {
-            fn to_sql(&self) -> String {
-                "".to_string()
-            }
-        }
-
-    )
-)
-
 impl ToSql for ()  {
     fn to_sql(&self) -> String {
         "DEFAULT VALUES".to_string()
     }
 }
-
-to_sql_for_insert_tuple!("{}", T1, t1)
-to_sql_for_insert_tuple!("{}, {}", T1, t1, T2, t2)
 
 impl<T: Clone, V: ToSql, M: Clone> ToSql for Insert<T, V, M> {
     fn to_sql(&self) -> String {
@@ -66,6 +48,6 @@ impl<T: Clone, V: ToSql, M: Clone> ToSql for Insert<T, V, M> {
 impl<T: Clone, V: Clone+ToSql, M: Clone> ToSql for InsertQuery<T, V, M> {
     fn to_sql(&self) -> String {
         // ..
-        format!("{}", self.get_values().to_sql())
+        format!("{}", self.values.to_sql())
     }
 }
